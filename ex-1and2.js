0680376abaeaeb96eff1e423df87fb5e95773193
Phuree Limphornpugdee <phureelimphornpugdee@Phurees-MacBook-Air.local>
2023-12-11 20:38:47 +0700

    feat: excercise value-variable by Tar

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let personFirstName = "Karter";
+let personLastName = "Anderson";
+let personAddress =  "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const activeRestaurant = 1500;
+
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+personAddress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+activeRestaurant = 2500;
+
